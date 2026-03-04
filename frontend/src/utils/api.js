bc68fd97d77d146091c308ae58c25abbec2b2b2f
Hemanth-1354 <hemanthkumarpolisetti@gmail.com>
2026-03-05 02:08:12 +0530

    Deployment configuration-url -3

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
+base_url = process.env.NEXT_PUBLIC_API_URL
+// # Set this in your .env file, e.g. REACT_APP_API_URL=http://localhost:8000
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: `${base_url}/api`,
   timeout: 900000, // 15 minutes
 })
 
@@ -18,3 +20,27 @@ api.interceptors.response.use(
 )
 
 export default api
+
+
+
+
+
+// // import axios from 'axios'
+
+// // const api = axios.create({
+// //   baseURL: process.env.NEXT_PUBLIC_API_URL, // Use your backend
+// //   timeout: 900000, // 15 minutes
+// // })
+
+// api.interceptors.request.use(config => {
+//   const token = localStorage.getItem('token')
+//   if (token) config.headers.Authorization = `Bearer ${token}`
+//   return config
+// })
+
+// api.interceptors.response.use(
+//   res => res,
+//   err => Promise.reject(err)
+// )
+
+// export default api
